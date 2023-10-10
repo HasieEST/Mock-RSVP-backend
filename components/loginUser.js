389f24eeedc16e1077c7450cbfe544c32ec0a39e
Ayrton Mägipõld <ayrtonm0@gmail.com>
2023-10-10 23:27:38 +0300

    Creation and Authentication of Users.

--- components/loginUser.js
+++ components/loginUser.js
@@ -5,24 +5,20 @@ import { config } from 'dotenv';
 config();
 
 const loginUser = async (username, password) => {
-    try {
-        const userData = await getUserData(username);
+    const userData = await getUserData(username);
 
-        if (userData.success) {
-            const { hashedPassword, salt } = userData;
-            const passwordMatches = await verifyPassword(password, salt, hashedPassword);
+    if (userData.success) {
+        const { hashedPassword, salt } = userData;
+        const passwordMatches = await verifyPassword(password, salt, hashedPassword);
 
-            if (passwordMatches) {
-                const token = jwt.sign({ username }, process.env.JWT_TOKEN, { expiresIn: '1h' });
-                return { success: true, token };
-            } else {
-                return { success: false, message: 'Invalid password' };
-            }
+        if (passwordMatches) {
+            const token = jwt.sign({ username }, process.env.JWT_TOKEN, { expiresIn: '1h' });
+            return { success: true, token };
         } else {
-            return { success: false, message: 'User not found' };
+            return { success: false, message: 'Invalid password' };
         }
-    } catch (error) {
-        return { success: false, message: 'Catch error:', error };
+    } else {
+        return { success: false, message: 'User not found' };
     }
 };
 
--- server.js
+++ server.js
@@ -10,53 +10,57 @@ const server = express();
 server.use(express.json());
 
 server.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-    registerUser(username, email, password, (error, results) => {
-        if (error) {
-            let statusCode = 500; // Default status for unexpected errors
-            let message = 'Registration failed.';
+  const { username, email, password } = req.body;
+  registerUser(username, email, password, (error, results) => {
+    if (error) {
+      let statusCode = 500; // Default status for unexpected errors
+      let message = 'Registration failed.';
 
-            if (error.message === 'Duplicate username and email found') {
-                statusCode = 400;
-                message = 'Duplicate username and email found';
-            } else if (error.message === 'Duplicate username found') {
-                statusCode = 400;
-                message = 'Duplicate username found';
-            } else if (error.message === 'Duplicate email found') {
-                statusCode = 400;
-                message = 'Duplicate email found';
-            }
+      if (error.message === 'Duplicate username and email found') {
+        statusCode = 400;
+        message = 'Duplicate username and email found';
+      } else if (error.message === 'Duplicate username found') {
+        statusCode = 400;
+        message = 'Duplicate username found';
+      } else if (error.message === 'Duplicate email found') {
+        statusCode = 400;
+        message = 'Duplicate email found';
+      }
 
-            res.status(statusCode).send(message);
-        } else {
-            res.status(200).send('Registration successful.');
-        }
-    });
+      res.status(statusCode).send(message);
+    } else {
+      res.status(200).send('Registration successful.');
+    }
+  });
 });
 
 
 server.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const loginResult = await loginUser(username, password);
-  
-      if (loginResult.success) {
-        res.status(200).json({ success: true, token: loginResult.token });
-      } else {
-        res.status(401).json({ success: false, message: loginResult.message });
-      }
-    } catch (error) {
-      console.error('An error occurred during login:', error);
-      res.status(500).json({ success: false, message: 'An error occurred during login' });
+  const { username, password } = req.body;
+
+  try {
+    const loginResult = await loginUser(username, password);
+
+    if (loginResult.success) {
+      res.status(200).json({ success: true, token: loginResult.token });
+    } else {
+      res.status(401).json({ success: false, message: loginResult.message });
     }
-  });
-  
+  } catch (error) {
+    if (error.message === 'User not found') {
+      res.status(401).json({ success: false, message: error.message });
+    } else {
+      console.error('An error occurred during login:', error);
+      res.status(500).json({ success: false, message: error.message });
+    }
+  }
+});
+
 
 
 
 const app = server.listen(3006, () => {
-    console.log('Server started on port 3006')
+  console.log('Server started on port 3006')
 })
 
 export default app;
